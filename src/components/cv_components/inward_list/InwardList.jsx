88932b4b6fd681011930e0d10adf3003c855eedf
Subaash1706 <subaashbala@gmail.com>
2023-12-31 17:37:05 +0530

    style: improve UI and responsiveness

--- src/components/cv_components/inward_list/InwardList.jsx
+++ src/components/cv_components/inward_list/InwardList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './list.module.css'
+import FlexBox from '../flexbox/FlexBox'
 
 // Accepted format: 
 //                  (i) props.object=false -> [ 'item 1', 'item 2', 'item 3' ] 
@@ -11,7 +12,18 @@ function InwardList(props) {
   return (
     <ul className={classes.inwardList}>
         { !object ? itemsArray.map((ele, ind)=><li key={ind}>{ele}</li>) : 
-         listItems[0].map((item, ind)=><li key={`${ind}${item.name}`}>{ item.name }</li>) }
+         listItems[0].map((item, ind)=>
+         <React.Fragment key={`${ind}${item.name}`}>
+        <FlexBox direction='row' style={{justifyContent: 'space-between'}}>
+          <li key={`${ind}${item.name}`}>
+            { item.name }
+          </li>
+          { props.date?
+                <span className={classes.date}>{ item.date }</span>
+                : '' 
+          }
+        </FlexBox></React.Fragment>)
+        }
     </ul>
   )
 }
