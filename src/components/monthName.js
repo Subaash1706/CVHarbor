e30f8a121c3909e3b738b2d835ba36bdb3ae9538
Subaash1706 <subaashbala@gmail.com>
2023-12-31 17:37:55 +0530

    build: add module to return month names

---
+++ src/components/monthName.js
@@ -0,0 +1,4 @@
+
+    // date-format = MM-YYYY
+const monthNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septembter', 'October', 'November', 'December' ]
+export default monthNames
