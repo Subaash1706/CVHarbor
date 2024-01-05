0a7e98eca8453cd7c02c0a9908e59720c9f11db5
Subaash1706 <subaashbala@gmail.com>
2024-01-05 23:05:05 +0530

    remove excess content

--- src/components/landing/Images.js
+++
@@ -1,7 +0,0 @@
-import image1 from '../../assets/images/sample/Arrayfrom.png'
-import image2 from '../../assets/images/sample/groupBy.jpeg'
-import image3 from '../../assets/images/sample/OBjectgroupBy.png'
-import image4 from '../../assets/images/sample/ss.jpg'
-import image5 from '../../assets/images/sample/Tweelet.png'
-
-export { image1, image2, image3, image4, image5 }
