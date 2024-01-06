0019c3fc16fe9d666cce7213cd265ee46ab270ee
Subaash1706 <subaashbala@gmail.com>
2024-01-06 21:59:48 +0530

    fix: file path

--- src/components/imageSelector.js
+++ src/components/imageSelector.js
@@ -2,7 +2,7 @@ const fileName = [ 'Harvard1.jpg', 'Harvard2.jpg', 'Harvard3.jpg', 'Harvard4.jpg
 const fileName2 = [ 'Mit1.jpg', 'Mit2.jpg', 'Mit3.jpg', 'Mit4.jpg', 'Mit5.PNG' ]
 const fileName3 = [ 'Modern1.PNG', 'Modern2.png' ]
 
-const harvardFileArray = fileName.map((item, ind)=>`/assets/images/templates/Harvard/${item}`)
+const harvardFileArray = fileName.map((item, ind)=>`/cvharbor/assets/images/templates/Harvard/${item}`)
 const mitFileArray = fileName2.map((item, ind)=>`/assets/images/templates/Mit/${item}`)
 const modernFileArray = fileName3.map((item, ind)=>`/assets/images/templates/Modern/${item}`)
 
