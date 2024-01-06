d0df3dcdd29cb8d08b59dd41c17b6e607cc9b5f5
Subaash1706 <subaashbala@gmail.com>
2024-01-06 22:07:10 +0530

    fix: preview image paths

--- src/components/imageSelector.js
+++ src/components/imageSelector.js
@@ -3,7 +3,7 @@ const fileName2 = [ 'Mit1.jpg', 'Mit2.jpg', 'Mit3.jpg', 'Mit4.jpg', 'Mit5.PNG' ]
 const fileName3 = [ 'Modern1.PNG', 'Modern2.png' ]
 
 const harvardFileArray = fileName.map((item, ind)=>`/cvharbor/assets/images/templates/Harvard/${item}`)
-const mitFileArray = fileName2.map((item, ind)=>`/assets/images/templates/Mit/${item}`)
-const modernFileArray = fileName3.map((item, ind)=>`/assets/images/templates/Modern/${item}`)
+const mitFileArray = fileName2.map((item, ind)=>`/cvharbor/assets/images/templates/Mit/${item}`)
+const modernFileArray = fileName3.map((item, ind)=>`/cvharbor/assets/images/templates/Modern/${item}`)
 
 export { harvardFileArray, mitFileArray, modernFileArray }
